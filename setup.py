1efaedf9f73d80a681c910569dce01db8d8e5503
brennv <brennv@users.noreply.github.com>
2017-04-09 23:01:15 -0700

    tweak build

--- setup.py
+++ setup.py
@@ -1,22 +1,39 @@
-#! /usr/bin/python
-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('VERSION') as f:
-      luminol_version = f.read().strip()
+    version = f.read().strip()
 
 with open('requirements.txt') as f:
-      required = f.read().splitlines()
+    required = f.read().splitlines()
 
-setup(name="luminol",
-      description='luminol is an anomaly detection and correlation library for timeseries data.',
-      url='https://github.com/linkedin/luminol',
-      author='Naarad Developers',
-      author_email='naarad-dev@googlegroups.com',
-      version=luminol_version,
-      packages=['luminol', 'luminol.algorithms', 'luminol.modules', 'luminol.algorithms.anomaly_detector_algorithms',
-                'luminol.algorithms.correlator_algorithms'],
-      package_dir={'': 'src'},
-      install_requires=required,
-      license='Apache 2.0',
-      )
+brief_description = """
+`https://github.com/brennv/luminol3
+<https://github.com/brennv/luminol3>`_.
+"""
+
+setup(
+    name='luminol3',
+    packages=['luminol'],
+    version=version,
+    description='Anomaly detection for time series data',
+    long_description=brief_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'],
+    author='brennv',
+    author_email='brennan@beta.build',
+    license='Apache 2.0',
+    url='https://github.com/brennv/luminol3',
+    download_url='https://github.com/brennv/luminol3/tarball/' + version,
+    keywords='luminol anomaly detection time series data signal processing',
+    install_requires=required,
+    include_package_data=True,
+    zip_safe=False)
